099b7be83b5134cc756a56630480d2344c6e287f
Aliaksandr Senkevich <puskin1914@gmail.com>
2020-08-03 11:43:52 +0300

    feat: add navbar

---
+++ src/components/TopBar/TopBar.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+export const TopBar = () => {
+  return (
+    <nav className="navbar navbar-light">
+      <div className="container">
+        <Link to="/" className="navbar-brand">
+          Medium
+        </Link>
+        <ul className="nav navbar-nav pull-xs-right">
+          <li className="nav-item">
+            <NavLink to="/" className="nav-link" exact>
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/login" className="nav-link">
+              Sing in
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/register" className="nav-link">
+              Sing up
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import { Routes } from "./pages/Routes";
+import { TopBar } from "./components/TopBar";
 
 function App() {
   return (
     <div>
       <Router>
+        <TopBar />
         <Routes />
       </Router>
     </div>
