7a09c07acac3db56ebd45e88e478f516c99ebdf8
Aliaksandr Senkevich <puskin1914@gmail.com>
2020-08-06 14:09:30 +0300

    feat: useRef example

--- src/pages/Authentication/Authentication.js
+++ src/pages/Authentication/Authentication.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import { Link } from "react-router-dom";
 
 export const Authentication = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const emailRef = useRef(null);
+  console.log("Authentication -> emailRef", emailRef.current?.value);
+
+  const onChangeEmail = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const onChangePassword = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("submit");
+  };
+
   return (
     <div className="authPage">
       <div className="container page">
@@ -11,13 +29,16 @@ export const Authentication = () => {
             <p className="text-xs-center">
               <Link to="/register">Need an account?</Link>
             </p>
-            <form>
+            <form onSubmit={handleSubmit}>
               <fieldset>
                 <fieldset className="form-group">
                   <input
                     type="email"
                     className="form-control form-control-lg"
                     placeholder="Email"
+                    // value={email}
+                    // onChange={onChangeEmail}
+                    ref={emailRef}
                   />
                 </fieldset>
 
@@ -26,6 +47,8 @@ export const Authentication = () => {
                     type="password"
                     className="form-control form-control-lg"
                     placeholder="Password"
+                    value={password}
+                    onChange={onChangePassword}
                   />
                 </fieldset>
 
