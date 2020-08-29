3758c8c3c4c857241a2a9d3eb328e74c587cabc3
Aliaksandr Senkevich <puskin1914@gmail.com>
2020-08-29 22:29:39 +0300

    feat: add Feed

--- src/pages/GlobalFeed/GlobalFeed.js
+++ src/pages/GlobalFeed/GlobalFeed.js
@@ -1,5 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useFetch } from '../../hooks/useFetch';
+import { Feed } from '../../components/Feed';
 
 export const GlobalFeed = () => {
-  return <div>GlobalFeed</div>;
+  const apiUrl = '/articles?limit=10&offset=0';
+  const [{ response, error, isLoading }, doFetch] = useFetch(apiUrl);
+  console.log('res', response, error, isLoading);
+
+  useEffect(() => {
+    doFetch();
+  }, [doFetch]);
+
+  return (
+    <div className="home-page">
+      <div className="banner">
+        <h1>Medium Clone</h1>
+        <p>A place to share knowledge</p>
+      </div>
+      <div className="container page">
+        <div className="row">
+          <div className="col-md-9">
+            {isLoading && <div>Loading...</div>}
+            {error && <div>Some error happened</div>}
+            {!isLoading && response && (
+              <>
+                <Feed articles={response.articles} />
+              </>
+            )}
+          </div>
+          <div className="col-md-3">Popular tags</div>
+        </div>
+      </div>
+    </div>
+  );
 };
